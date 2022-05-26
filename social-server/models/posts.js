b163f8dd191c09bb3bd3c5f34ad925d7eaac2e97
rjhelm <rjhelm@github.io>
2022-05-26 17:36:21 -0600

    created models for user, posts, comments

---
+++ social-server/models/posts.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const LikeSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+});
+
+const ImageSchema = new mongoose.Schema({
+    image: {
+        type: String,
+    },
+});
+
+const PostSchema = new mongoose.Schema({
+    caption: {
+        type: String,
+    },
+    images: [ImageSchema],
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    likes: [LikeSchema],
+    
+}, { timestamps: true });
+
+const Post = mongoose.model('Post', PostSchema);
+export default Post;
---
+++ social-server/models/users.js
@@ -0,0 +1,106 @@
+import mongoose from 'mongoose';
+import PassportLocal from 'passport-local-mongoose';
+
+const SocialSchema = new mongoose.Schema({
+    platform: {
+        type: String,
+    },
+    link: {
+        type: String,
+        required: true,
+    }
+});
+
+const EduSchema = new mongoose.Schema({
+    institute: {
+        type: String,
+        required: true,
+    },
+    desc: {
+        type: String,
+    },
+    from: {
+        type: String,
+    },
+    to: {
+        type: String,
+    },
+    present: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const FollowersSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    full_name: {
+        type: String,
+        required: true,
+    },
+    dp: {
+        type: String,
+    },
+}, { timestamps: true });
+
+const FollowingSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    full_name: {
+        type: String,
+        required: true,
+    },
+    dp: {
+        type: String,
+    },
+}, { timestamps: true });
+
+const UserSchema = new mongoose.Schema({
+    full_name: {
+        type: String,
+        default: '',
+    },
+    dp: {
+        type: String,
+    },
+    cover: {
+        type: String,
+    },
+    view: {
+        type: Number,
+        default: 0,
+    },
+    email: {
+        type: String,
+    },
+    bio: {
+        type: String,
+    },
+    relationship: {
+        type: String,
+    },
+    gender: {
+        type: String,
+    },
+    social: [SocialSchema],
+    education: [EduSchema],
+    following: [FollowingSchema],
+    followers: [FollowersSchema],
+}, { timestamps: true });
+
+UserSchema.plugin(PassportLocal);
+
+const User = mongosose.model('User', UserSchema);
+export default User;
